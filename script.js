cc8bd0e4d5ac5c371f370e66ce1796a925b603f1
Boecxken <84395269+Boecxken@users.noreply.github.com>
2021-06-06 11:36:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const makeSushi = function () {
+    console.log("Kook de rijst");
+    console.log("Snijd ingrediënten");
+    console.log("Leg de ingrediënten op de rijst");
+    console.log("Rol de sushi");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+const makePizza = function () {
+    console.log("Maak pizzadeeg");
+    console.log("Maak de saus");
+    console.log("Beleg de pizza");
+    console.log("Bak de pizza in de oven");
+};
+
+makePizza();
+makePizza();
+
